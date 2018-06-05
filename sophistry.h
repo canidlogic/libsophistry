06f2f6315fd4e0908d8b7203d70d1ea41dc4c39a
Noah Johnson <noah.johnson@loupmail.com>
2018-06-05 15:41:05 -0500

    Defined the scanline import function

--- sophistry.h
+++ sophistry.h
@@ -25,14 +25,18 @@
  * 
  * sphy_int is a signed integer type that must be at least 32-bit.
  * 
+ * sphy_u8 is an unsigned, 8-bit integer that is used to represent
+ * bytes.
+ * 
  * The definitions given here use the <stdint.h> header.  If this
  * header is not available, comment out the include below and replace
- * the uint32_t int32_t in the definitions below with whatever are the
- * appropriate types for the compiler.
+ * the uint32_t, int32_t, and uint8_t in the definitions below with
+ * whatever are the appropriate types for the compiler.
  */
 #include <stdint.h>
 typedef uint32_t sphy_u32;    /* Unsigned integer, exactly  32-bit */
 typedef int32_t  sphy_int;    /* Signed   integer, at least 32-bit */
+typedef uint8_t  sphy_u8 ;    /* Unsigned integer, exactly   8-bit */
 
 /*
  * The maximum number of samples in an object.
@@ -43,20 +47,23 @@ typedef int32_t  sphy_int;    /* Signed   integer, at least 32-bit */
 #define SPHY_MAXCOUNT ((sphy_int) 268435456L)
 
 /*
- * Structure prototype for SPHY_SCANLINE.
+ * Structure prototypes.
  * 
- * The actual structure is defined in the implementation file.
+ * The actual structures are defined in the implementation file.
  */
 struct SPHY_SCANLINE_TAG;
 typedef struct SPHY_SCANLINE_TAG SPHY_SCANLINE;
 
-/*
- * Structure prototype for SPHY_AUDIOBUF.
- * 
- * The actual structure is defined in the implementation file.
- */
 struct SPHY_AUDIOBUF_TAG;
 typedef struct SPHY_AUDIOBUF_TAG SPHY_AUDIOBUF;
+
+struct SPHY_ISRCFMT_TAG;
+typedef struct SPHY_ISRCFMT_TAG SPHY_ISRCFMT;
+
+/*
+ * Status code definitions.
+ */
+#define SPHY_OK         ( 0)  /* Operation successful */
 
 /*
  * Constructor for a scanline.
@@ -107,6 +114,53 @@ void sphy_scanline_free(SPHY_SCANLINE *psl);
  *   a pointer to the internal samples
  */
 sphy_u32 *sphy_scanline_ptr(SPHY_SCANLINE *psl);
+
+/*
+ * Import decoded pixels from binary data into a scanline object.
+ * 
+ * psl is the scanline object to write the pixels into, while psrc
+ * points to the bytes of data that shall be decoded.
+ * 
+ * offs is the zero-based index to begin writing encoded pixels to
+ * within the scanline.  Passing zero means decoded pixels are written
+ * starting at the first sample of the scanline.  offs must be at least
+ * zero and less than the width, as determined by sphy_scanline_count.
+ * 
+ * src_fmt describes the format of the pixels that are encoded in the
+ * binary data.
+ * 
+ * src_len is the length of the binary data in bytes.  It must be
+ * greater than zero.  Its length must be such that there are no
+ * partially encoded pixels at the end of the binary data.  Furthermore,
+ * the number of pixels included in the binary data may not exceed
+ * (width - offs), where width is the width of the scanline in pixels
+ * and offs is the offs parameter.
+ * 
+ * The return value is SPHY_OK if successful, else one of the error
+ * status codes.
+ * 
+ * Parameters:
+ * 
+ *   psl - the scanline object to import pixels into
+ * 
+ *   offs - the scanline offset to begin writing the imported pixels
+ * 
+ *   psrc - pointer to the binary data to decode pixels from
+ * 
+ *   src_len - the length of the binary data in bytes
+ * 
+ *   src_fmt - the format of the binary data to import from
+ * 
+ * Return:
+ * 
+ *   SPHY_OK if successful, else one of the error status codes
+ */
+sphy_int sphy_scanline_import(
+    SPHY_SCANLINE *      psl,
+    sphy_int             offs,
+    const sphy_u8      * psrc,
+    sphy_int             src_len,
+    const SPHY_ISRCFMT * src_fmt);
 
 /*
  * Return the width in pixels of the given scanline.
